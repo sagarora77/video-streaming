8956a72511dd8724e23e2fc22c6035d0719d77a3
Sagar.Arora <sagar.arora@flintmail.com>
2020-06-06 23:27:38 +0530

    port issue fixed

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('/video', function(req, res) {
     }
 })
 
-app.listen(3001, function() {
-    console.log('Listening on port 3001!')
+app.listen(3000, function() {
+    console.log('Listening on port 3000!')
 })
