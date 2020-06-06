7a18a2f95746f2885d86749049e23848d9140f72
Sagar.Arora <sagar.arora@flintmail.com>
2020-06-07 00:18:54 +0530

    Procfile and package.json

--- server.js
+++ server.js
@@ -48,6 +48,7 @@ app.get('/video', function(req, res) {
     }
 })
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log('Listening on port ' + process.env.port ? process.env.port : 3000)
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
