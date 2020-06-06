a24910656d34c8856a00b0ca29448da34c80415a
Sagar.Arora <sagar.arora@flintmail.com>
2020-06-07 00:09:27 +0530

    Updated listen

--- server.js
+++ server.js
@@ -48,6 +48,6 @@ app.get('/video', function(req, res) {
     }
 })
 
-app.listen(3000, function() {
-    console.log('Listening on port 3000!')
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Listening on port ' + process.env.port ? process.env.port : 3000)
 })
